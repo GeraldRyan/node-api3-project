ffb494b6cc88317ab07e55ccb8a016cc6fd1b299
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-07 15:52:36 -0500

    got database imported

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code away!
+const express = require("express")
+const server = express()
+// const postRouter = require("./postrouter")
+
+server.use(express.json())
+
+server.get("/", (req, res)=>{
+  res.send("Server is running")
+})
+
+server.use("/api/posts", postRouter)  // passes postRouter as the function argument. Is this middleware? postRouter is imported from our own file. 
+
+server.listen(5000)
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
+const postDB = require("./posts/postDb")
+const usersDB = require("./users/userDb")
 const server = express();
+
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +11,12 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+
+  `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+    'Origin'
+  )}`
+  next();
+}
 
 module.exports = server;
